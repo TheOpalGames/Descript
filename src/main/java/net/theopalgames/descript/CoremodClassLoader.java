43f5a52dde786422e40a49d2667e5a43b4153726
Hallowizer <40325350+Hallowizer@users.noreply.github.com>
2019-07-12 15:31:36 -0700

    Better delegation

--- src/main/java/net/theopalgames/descript/CoremodClassLoader.java
+++ src/main/java/net/theopalgames/descript/CoremodClassLoader.java
@@ -4,10 +4,20 @@ import java.net.URL;
 import java.security.CodeSource;
 import java.security.SecureClassLoader;
 
+import cpw.mods.modlauncher.Launcher;
+import cpw.mods.modlauncher.TransformingClassLoader;
+import net.theopalgames.descript.reflect.ReflectUtil;
+
 public final class CoremodClassLoader extends SecureClassLoader {
 	private final IClassLoaderDelegate delegate;
+	private final ClassLoader parent;
+	
+	public static final TransformingClassLoader gameLoader = ReflectUtil.readField(Launcher.class, "classLoader");
 	
 	public CoremodClassLoader() {
+		super(null);
+		
+		parent = getClass().getClassLoader();
 		delegate = new UclClassLoader().getDelegate();
 	}
 	
@@ -17,14 +27,23 @@ public final class CoremodClassLoader extends SecureClassLoader {
 	
 	@Override
 	protected Class<?> findClass(String name) throws ClassNotFoundException {
-		byte[] basicClass = delegate.getBytes(name);
-		CodeSource source = delegate.getCodeSource(name);
+		int lastDot = name.lastIndexOf('.');
 		
-//		Result result = SerializationTransformer.transform(basicClass);
-//		if (result.serialize)
-//			CoreModLoader.transformers.serialize.add(name);
-//		
-//		return defineClass(name, result.transformed, 0, result.transformed.length, source);
-		return defineClass(name, basicClass, 0, basicClass.length, source);
+		if (name.substring(0, lastDot).equals("net.theopalgames.descript"))
+			return parent.loadClass(name);
+		
+		try {
+			byte[] basicClass = delegate.getBytes(name);
+			CodeSource source = delegate.getCodeSource(name);
+			
+	//		Result result = SerializationTransformer.transform(basicClass);
+	//		if (result.serialize)
+	//			CoreModLoader.transformers.serialize.add(name);
+	//		
+	//		return defineClass(name, result.transformed, 0, result.transformed.length, source);
+			return defineClass(name, basicClass, 0, basicClass.length, source);
+		} catch (ClassNotFoundException e) {
+			return gameLoader.loadClass(name);
+		}
 	}
 }
